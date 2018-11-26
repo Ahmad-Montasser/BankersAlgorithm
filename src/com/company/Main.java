89f22887ec3a7d818bb4267af6de0481c4e079d6
Ahmad-Montasser <ahmad.montasser@yahoo.com>
2018-11-26 15:03:22 +0200

    Solved Infinite loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,46 +9,59 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number Of processes");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // number of processes
         System.out.println("Enter Number Of resources");
-        int m = sc.nextInt();
+        int m = sc.nextInt(); // number of resources
         Process[] processes = new Process[n];
-        int[] maxResources = new int[m];
-        int[] remainingResources ;
-        ArrayList<String> result = new ArrayList<>(n);
-        for (int i=0;i<m ; i++) {
-            System.out.println("Please Enter Max Number of Resource");
-            maxResources[i] = sc.nextInt();
+        int[] maxResources = new int[m]; // the max instances of resources of each resource type
+        int[] remainingResources; // the unallocated resources of each resource type
+        ArrayList<Process> result = new ArrayList<>(n); // the final pattern of the execution
+        System.out.print("Please Enter Max Number of Resources ");
+        for (int i = 0; i < m; i++) {
+            maxResources[i] = sc.nextInt(); // gets the maximum instances of each resource
         }
-        int x;
-        remainingResources=maxResources;
+        remainingResources = maxResources;
         for (int i = 0; i < n; i++) {
-            System.out.println("Please enter Allocated & max resources");
-            int[] allocated = new int[m];
-            int[] max = new int[m];
+            int[] allocated = new int[m]; // gets the allocated instances of each resource by each process
+            int[] max = new int[m]; // gets the maximum instances of resources that the process wants to finish execution
+            System.out.print("Enter allocated instances of resources for process " + i + " :");
             for (int j = 0; j < m; j++) {
                 allocated[j] = sc.nextInt();
-                remainingResources[j] -= allocated[j];
-                max[j] = sc.nextInt();
+                remainingResources[j] -= allocated[j]; // subtracts the allocated instances from the total unallocated resource
+            }
+            System.out.print("Enter the maximum instances needed of resources for process " + i + " :");
+            for(int j = 0; j < m; j++) {
+                max[j] = sc.nextInt(); // saves the max instances that the process require
             }
             processes[i] = new Process(allocated, max, i);
         }
         Process.setRemainingProcesses(n);
-        while (Process.remainingProcesses != 0) {
+        int finishedProcesses; // gets the value of the finished processes at the end and the last of the loop iteration
+        boolean validExecution = true; // indicates weather or not a pattern was found
+        while (Process.getRemainingProcesses() != 0) {
+            finishedProcesses = result.size(); // gets the number of finished processes
             for (Process p : processes) {
-                if (p.finished)
+                if (p.isFinished())
                     continue;
                 if (p.isSafe(remainingResources)) {
                     int[] freed = p.releaseResources();
                     for (int i = 0; i < m; i++) {
-                        remainingResources[i] += freed[i];
+                        remainingResources[i] += freed[i]; //add the allocated resources by the process to the total unallocated resources
                     }
-                    result.add("p"+p.processNumber);
+                    result.add(p);
                 }
             }
+            if(result.size() == finishedProcesses) { // compare the size of the finished processes with the initial size before the loop
+                validExecution = false;
+                break;
+            }
         }
-        for(String i : result)
-            System.out.print(i+" ");
-
+        if(validExecution) {
+            for (Process p : result)
+                System.out.print(p + " ");
+            System.out.println();
+        } else {
+            System.out.println("There's no valid pattern to finish these processes in the safe state");
+        }
     }
 }
