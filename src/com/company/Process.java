911b0b3d89020030b1b424dfb4a2d482e72e0f34
Mohamed Ahmed <motawfik1998@gmail.com>
2018-11-26 13:34:46 +0200

    Solved no pattern problem

--- src/com/company/Process.java
+++ src/com/company/Process.java
@@ -1,41 +1,50 @@
 package com.company;
 
 public class Process {
-    int[] allocated;
-    int[] required;
-    int[] max;
-    boolean finished=false;
-    static int remainingProcesses;
-    int processNumber;
+    private int[] allocated;
+    private int[] required;
+    private boolean finished = false;
+    private static int remainingProcesses;
+    private int processNumber;
 
     public Process(int[] allocated, int[] max, int processNumber) {
         this.allocated = allocated;
-        this.max = max;
         this.processNumber = processNumber;
         required = new int[allocated.length];
-        for (int i = 0; i< this.allocated.length; i++)
-        {
-            required[i]=max[i]-allocated[i];
-
+        for (int i = 0; i < this.allocated.length; i++) {
+            required[i] = max[i] - allocated[i];
         }
     }
 
-    public static void setRemainingProcesses(int remainingProcesses) {
-        Process.remainingProcesses = remainingProcesses;
-    }
-
-    public boolean isSafe(int[] remainingResources) {
-        for (int i = 0; i <allocated.length ; i++) {
-            if(remainingResources[i]>=(max[i]-allocated[i]))
+    public boolean isSafe(int[] remainingResources) { // determine weather the process could terminate safely without entering the unsafe state
+        for (int i = 0; i < allocated.length; i++) {
+            if (remainingResources[i] >= required[i])
                 continue;
             return false;
         }
         return true;
     }
 
-    public int[] releaseResources() {
-        finished=true;
+    public int[] releaseResources() { // returns the allocated resources by the processes (free them)
+        finished = true;
         remainingProcesses--;
         return allocated;
+    }
+
+    public static void setRemainingProcesses(int remainingProcesses) {
+        Process.remainingProcesses = remainingProcesses;
+    }
+
+    public static int getRemainingProcesses() {
+        return remainingProcesses;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    @Override
+    public String toString() {
+        return "p" + processNumber;
     }
 }
